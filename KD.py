0accbdae291e4a1c3f0d055539fb2ab0619e5cfa
Shiva Pokhrel <69740754+ShivaPokhrel@users.noreply.github.com>
2023-05-29 17:28:38 +1000

    Update KD.py

--- KD.py
+++ KD.py
@@ -8,7 +8,7 @@ from ._base import Distiller
 def kd_loss(logits_student, logits_teacher, temperature):
     log_pred_student = F.log_softmax(logits_student / temperature, dim=1)
     pred_teacher = F.softmax(logits_teacher / temperature, dim=1)
-    loss_kd = F.MSE(log_pred_student, pred_teacher, reduction="none").sum(1).mean()
+    loss_kd = F.MSELoss(log_pred_student, pred_teacher, reduction="none").sum(1).mean()
     loss_kd *= temperature**2
     return loss_kd
 
